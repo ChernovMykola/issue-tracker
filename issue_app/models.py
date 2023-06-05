df3a34469e83e0cd1a86991d612132527c32438a
ChernovMykola <4ernovnikola@gmail.com>
2023-06-05 18:43:41 +0200

    UPIModel

--- issue_app/models.py
+++ issue_app/models.py
@@ -7,3 +7,13 @@ class Issue(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     approve = models.BooleanField(default=False)
     approved_day = models.DateTimeField()
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    username = models.CharField(max_length=200, default='')
+    email = models.EmailField(max_length=254, default='')
+    password = models.CharField(max_length=200, default='')
+    confirm_password = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.user.username
