b43ae28d5cfafbea996d9f406dbf2bdcd1b72bc5
ChernovMykola <4ernovnikola@gmail.com>
2023-06-06 16:58:37 +0200

    UserForm

--- issue_app/forms.py
+++ issue_app/forms.py
@@ -1,2 +1,18 @@
+from django import forms
 
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import (
+    Issue,
+    UserProfileInfo
+)
+
+
+class UserForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = UserProfileInfo
+        fields = ('username', 'email', 'confirm_password')
 
