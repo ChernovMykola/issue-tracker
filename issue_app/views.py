c87ca903cffe6cdd70c420d892cd9017a0fe3f4a
ChernovMykola <4ernovnikola@gmail.com>
2023-06-08 14:40:24 +0200

    ICV

--- issue_app/views.py
+++ issue_app/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+)
 from .models import Issue
 from django.views.generic import (
     ListView,
     CreateView,
     DetailView
 )
+from .forms import IssueForm
 
 class IssueListView(ListView):
     model = Issue
@@ -15,10 +19,16 @@ class IssueListView(ListView):
 
 class IssueDetailView(DetailView):
     model = Issue
-    template_name = 'myissue/issue_detail.html'
+    template_name = 'issuetemp/issue_detail.html'
     context_object_name = 'issue'
 
 class IssueCreateView(CreateView):
     model = Issue
+    form_class = IssueForm
+    redirect_field_name = 'issuetemp/myissue'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
