9fe3cbbeeb2c563ac131f7ce30ad3d3a29f8bbdc
ChernovMykola <4ernovnikola@gmail.com>
2023-06-08 14:36:51 +0200

    IDV

--- issue_app/views.py
+++ issue_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Issue
 from django.views.generic import (
-    ListView
+    ListView,
+    CreateView,
+    DetailView
 )
 
 class IssueListView(ListView):
@@ -10,3 +12,13 @@ class IssueListView(ListView):
     def get_queryset(self):
         issue = Issue.objects.all()
         return issue
+
+class IssueDetailView(DetailView):
+    model = Issue
+    template_name = 'myissue/issue_detail.html'
+    context_object_name = 'issue'
+
+class IssueCreateView(CreateView):
+    model = Issue
+
+
